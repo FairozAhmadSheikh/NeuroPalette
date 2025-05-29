42937b2d1780f831fe4388f23cdb58dc2c47b8bd
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-05-29 17:21:25 +0530

    stylize image

--- models/style_transfer.py
+++ models/style_transfer.py
@@ -32,3 +32,67 @@ class ContentLoss(nn.Module):
     def forward(self, x):
         self.loss = nn.functional.mse_loss(x, self.target)
         return x
+    class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super().__init__()
+        self.target = self.gram_matrix(target_feature).detach()
+    def gram_matrix(self, x):
+        b, c, h, w = x.size()
+        features = x.view(c, h * w)
+        G = torch.mm(features, features.t())
+        return G.div(c * h * w)
+    def forward(self, x):
+        G = self.gram_matrix(x)
+        self.loss = nn.functional.mse_loss(G, self.target)
+        return x
+
+def stylize_image(content_path, style_path):
+    content = load_image(content_path)
+    style = load_image(style_path)
+    input_img = content.clone()
+
+    cnn = models.vgg19(pretrained=True).features.to(device).eval()
+    content_layers = ['conv_4']
+    style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+    content_losses = []
+    style_losses = []
+
+    model = nn.Sequential()
+    i = 0
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = f'conv_{i}'
+        elif isinstance(layer, nn.ReLU):
+            name = f'relu_{i}'
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = f'pool_{i}'
+        else:
+            continue
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            target = model(content).detach()
+            content_loss = ContentLoss(target)
+            model.add_module(f"content_loss_{i}", content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            target = model(style).detach()
+            style_loss = StyleLoss(target)
+            model.add_module(f"style_loss_{i}", style_loss)
+            style_losses.append(style_loss)
+
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], (ContentLoss, StyleLoss)):
+            break
+    model = model[:i+1]
+
+    input_img.requires_grad_(True)
+    optimizer = optim.LBFGS([input_img])
+
+    style_weight = 1e6
+    content_weight = 1
