77b5d94b07f28e61a695a892d87318cfe3c69cd2
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-05-29 17:20:31 +0530

    adding model code

--- models/style_transfer.py
+++ models/style_transfer.py
@@ -17,3 +17,18 @@ def load_image(img_path, max_size=512):
                              [0.229, 0.224, 0.225])
     ])
     return transform(image).unsqueeze(0).to(device)
+
+def im_convert(tensor):
+    image = tensor.clone().detach().cpu().squeeze(0)
+    image = image * torch.tensor([0.229, 0.224, 0.225]).view(3,1,1)
+    image = image + torch.tensor([0.485, 0.456, 0.406]).view(3,1,1)
+    image = image.clamp(0, 1)
+    return transforms.ToPILImage()(image)
+
+class ContentLoss(nn.Module):
+    def __init__(self, target):
+        super().__init__()
+        self.target = target.detach()
+    def forward(self, x):
+        self.loss = nn.functional.mse_loss(x, self.target)
+        return x
